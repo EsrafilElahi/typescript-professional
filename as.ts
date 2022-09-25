e23fa942a49e9d0f5566644acc322afe2754941d
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-09-25 12:12:01 +0330

    change

--- as.ts
+++ as.ts
@@ -1,6 +1,6 @@
 const fuck: any = "20";
 const fucker1 = fuck as string; // be onvan string
-const fucker2 = fuck as number; // be onvan string
+const fucker2 = fuck as number; // be onvan number
 
 let un: unknown;
 un = "motherFucker";
