4f6418aafff65ca0e88ee9df82e3a783d87a9558
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2023-05-14 12:18:54 +0330

    Create Generic.ts

---
+++ Generic.ts
@@ -0,0 +1,15 @@
+
+type GenericSelectProps<TValue> = {
+  formatLabel: (value: TValue) => string;
+  values: Readonly<TValue[]>;
+};
+  
+interface SingleSelectProps<TValue> extends GenericSelectProps<TValue> {
+  isMulti: false; // false, not boolean. For single select component this is always false
+  onChange: (value: TValue) => void;
+}
+  
+interface MultiSelectProps<TValue> extends GenericSelectProps<TValue> {
+  isMulti: true; // true, not boolean. For multi select component this is always true
+  onChange: (value: TValue[]) => void;
+}
