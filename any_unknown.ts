617b4913614ca66f9c21afeb6cef9b7831275de5
esrafilelahi <essinho1121@gmail.com>
2023-06-02 20:49:10 +0330

    unknonwn type description

--- any_unknown.ts
+++ any_unknown.ts
@@ -10,3 +10,4 @@ lastName = true
 
 let fName: string = firstName // no error ---> in any doesn't type-checking (assignable in any type any variable)
 let lName: string = lastName // error ---> in unknown does type-checking (doesn't assignable in any type any variable - just once)
+// first it's unknown -> until it's unknown it's assignable then find-out what's it's type - then can't assignable again
