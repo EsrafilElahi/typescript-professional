240af34a5876185bde8f29f1671f0c9eda7ff914
esrafilelahi <essinho1121@gmail.com>
2023-06-02 20:46:46 +0330

    any unknown

---
+++ any_unknown.ts
@@ -0,0 +1,12 @@
+let firstName: any;
+firstName = 'tom'
+firstName = 25
+firstName = false
+
+let lastName: unknown;
+lastName = 'hardy'
+lastName = 35
+lastName = true
+
+let fName: string = firstName // no error ---> in any doesn't type-checking (assignable in any type any variable)
+let lName: string = lastName // error ---> in unknown does type-checking (doesn't assignable in any type any variable - just once)
